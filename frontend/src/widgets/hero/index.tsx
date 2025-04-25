032591da4c21a2fa1f1d03b507e3e6f298cb8a7f
cm3tahkuh <markplay13498@gmail.com>
2025-04-25 15:58:24 +0500

    main section was done

---
+++ frontend/src/widgets/hero/index.tsx
@@ -0,0 +1,32 @@
+import { Heading, Text, Button, Flex } from "@radix-ui/themes";
+import styles from "./hero.module.scss";
+
+export const Hero: React.FC = () => {
+  return (
+    <Flex
+      direction="column"
+      align="center"
+      gap="4"
+      style={{ position: "relative", padding: "200px 20px" }}
+      pb="9"
+    >
+      <div className={`${styles.info__background} ${styles.ibg}`}>
+        <img src="images/backgroundHero.jpg" alt="Фон" />
+      </div>
+      <Heading size="9" style={{ color: "white" }} as="h1">
+        Винил - это круто.
+      </Heading>
+      <Text as="p" size="5" style={{ color: "white" }}>
+        Лучшие и качественные записи в нашем магазине
+      </Text>
+      <Flex gap="4">
+        <Button size="3" variant="classic">
+          Купить
+        </Button>
+        <Button size="3" style={{ color: "white" }} variant="soft">
+          Подробнее
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
